8f3dfa4d2e693f7aca6b10611478898e4d95a768
katya-sy <katya_sy_28@mail.ru>
2023-11-26 16:03:05 +0900

    create tabs

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const tabs = document.querySelectorAll(".tabheader__item"),
+  tabsNav = document.querySelector(".tabheader__items"),
+  tabsContent = document.querySelectorAll(".tabcontent");
+let tabheaderItemActive = document.querySelector(".tabheader__item_active");
+
+function hideTabsContent() {
+  tabsContent.forEach((item) => {
+    item.style.display = "none";
+  });
+
+  tabs.forEach((item, i) => {
+    if (item.classList.contains("tabheader__item_active")) {
+      tabsContent[i].style.display = "block";
+    }
+  });
+}
+
+hideTabsContent();
+
+tabsNav.addEventListener("click", (e) => {
+  if (
+    e.target &&
+    e.target.classList.contains("tabheader__item") &&
+    e.target !== tabheaderItemActive
+  ) {
+    tabheaderItemActive.classList.remove("tabheader__item_active");
+    tabheaderItemActive = e.target;
+    tabheaderItemActive.classList.add("tabheader__item_active");
+    hideTabsContent();
+  }
+});
