cfa4396db051cb3cff860f10bf8efccb6c4c4b00
katya-sy <katya_sy_28@mail.ru>
2024-01-25 00:12:46 +0900

    calculator of calories (basic)

--- js/script.js
+++ js/script.js
@@ -369,3 +369,78 @@ sliderIndicatorDots.forEach((dot) => {
     toActiveSlide();
   });
 });
+
+// calculator
+const result = document.querySelector(".calculating__result span");
+let gender = "female",
+  height,
+  weight,
+  age,
+  ratio = 1.375;
+
+function calcCalories() {
+  if (!gender || !height || !weight || !age || !ratio) {
+    result.textContent = "____";
+    return;
+  }
+
+  if (gender === "female") {
+    result.textContent = Math.round(
+      (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+    );
+  } else {
+    result.textContent = Math.round(
+      (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+    );
+  }
+}
+
+function getStaticInfo(parentSelector, activeClass) {
+  const elements = document.querySelectorAll(`${parentSelector} div`);
+
+  elements.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      if (e.target.getAttribute("data-ratio")) {
+        ratio = +e.target.getAttribute("data-ratio");
+      } else {
+        gender = e.target.getAttribute("id");
+      }
+
+      elements.forEach((element) => {
+        e.target === element
+          ? element.classList.add(activeClass)
+          : element.classList.remove(activeClass);
+      });
+
+      calcCalories();
+    });
+  });
+}
+
+function getDynamicInfo(selector) {
+  const input = document.querySelector(selector);
+
+  input.addEventListener("input", () => {
+    switch (input.getAttribute("id")) {
+      case "height":
+        height = +input.value;
+        break;
+      case "weight":
+        weight = +input.value;
+        break;
+      case "age":
+        age = +input.value;
+        break;
+    }
+    calcCalories();
+  });
+}
+
+calcCalories();
+
+getStaticInfo("#gender", "calculating__choose-item_active");
+getStaticInfo(".calculating__choose_big", "calculating__choose-item_active");
+
+getDynamicInfo("#height");
+getDynamicInfo("#weight");
+getDynamicInfo("#age");
